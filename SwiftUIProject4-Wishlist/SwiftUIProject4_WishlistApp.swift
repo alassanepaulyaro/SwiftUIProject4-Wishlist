e1c2685fddd12ff600b51a23566b5a750334caa4
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-12-03 09:06:08 +0100

    Initial Commit

---
+++ SwiftUIProject4-Wishlist/SwiftUIProject4_WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProject4_WishlistApp.swift
+//  SwiftUIProject4-Wishlist
+//
+//  Created by Yaro Paul  on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProject4_WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
