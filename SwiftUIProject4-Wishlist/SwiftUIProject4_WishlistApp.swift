b9abda62ffe4f757fa64f99166781404e431ad92
alassanepaulyaro <alassane-paul.yaro@outlook.fr>
2024-12-04 12:38:52 +0100

    project setup (#1)
    * project setup
    
    * model container and context
    
    * preview sample data
    
    * saving data
    
    * deleting data
    
    ---------
    
    Co-authored-by: Paul Yaro <“alassane-paul.yaro@outlook.fr”>

--- SwiftUIProject4-Wishlist/SwiftUIProject4_WishlistApp.swift
+++ SwiftUIProject4-Wishlist/SwiftUIProject4_WishlistApp.swift
@@ -5,6 +5,7 @@
 //  Created by Yaro Paul  on 03/12/2024.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -12,6 +13,7 @@ struct SwiftUIProject4_WishlistApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Wish.self)
         }
     }
 }
---
+++ SwiftUIProject4-Wishlist/WishModel.swift
@@ -0,0 +1,18 @@
+//
+//  WishModel.swift
+//  SwiftUIProject4-Wishlist
+//
+//  Created by Yaro Paul  on 03/12/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Wish {
+    var title: String
+
+    init(title: String) {
+        self.title = title
+    }
+}
